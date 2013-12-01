f43221c0cf25e9738577785a61bd905fb8c3b766
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-12-01 19:39:00 +1200

    base tests

--- verification/tests.py
+++ verification/tests.py
@@ -11,26 +11,31 @@ Each test is dict with
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": ["""DREAMING of apples on a wall,
+And dreaming often, dear,
+I dreamed that, if I counted all,
+-How many would appear?""", u"ten"],
+            "answer": [2, 14, 2, 16],
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": ["""He took his vorpal sword in hand:
+Long time the manxome foe he sought--
+So rested he by the Tumtum tree,
+And stood awhile in thought.
+And as in uffish thought he stood,
+The Jabberwock, with eyes of flame,
+Came whiffling through the tulgey wood,
+And burbled as it came!""", "noir"],
+            "answer": [4, 16, 7, 16],
         }
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": ["""DREAMING of apples on a wall,
+And dreaming often, dear,
+I dreamed that, if I counted all,
+-How many would appear?""", u"ten"],
+            "answer": [2, 14, 2, 16],
         },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
     ]
 }
